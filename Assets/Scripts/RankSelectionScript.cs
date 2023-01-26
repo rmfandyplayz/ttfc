8ef21850f7ef88abf9834f4479e54e39b018b59a
RMF-AndyPlayz <andyjia2500@gmail.com>
2023-01-25 16:37:16 -0800

    Testing release

--- Assets/Scripts/RankSelectionScript.cs
+++ Assets/Scripts/RankSelectionScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RankSelectionScript : MonoBehaviour
 {
+    public RectTransform scrollView;
+    private void Start()
+    {
+        scrollView.sizeDelta= new Vector2(scrollView.sizeDelta.x, scrollView.sizeDelta.x * Screen.height/ Screen.width);
+    }
     public void RankSelected(string rank)
     {
         RequirementsHandler.rank = (Requirement.Rank)System.Enum.Parse(typeof(Requirement.Rank), rank);
