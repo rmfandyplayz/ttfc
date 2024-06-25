09a366a31b45f6a6e81c58857f7fe49a2619cea0
RMF-AndyPlayz <andyjia2500@gmail.com>
2024-06-24 21:57:28 -0700

    Revert "e"
    This reverts commit 88f667936019e6c8e900ae86ff79adaa9ea12519.

--- Assets/Scripts/Scene-Specific Scripts/Check Updates Scene/UpdateChecker.cs
+++ Assets/Scripts/Scene-Specific Scripts/Check Updates Scene/UpdateChecker.cs
@@ -6,48 +6,22 @@ using UnityEngine.Events;
 
 public class UpdateChecker : MonoBehaviour
 {
-    [SerializeField] Canvas checkVersionUpdateCanvas;
-    [SerializeField] Canvas outOfDateCanvas;
-    [SerializeField] Canvas noInternetCanvas;
-
     string versionURL = "https://version.ttfc.zip";
     bool? isOutdated;
-    UnityEvent<bool> OnVersionCheckComplete = new UnityEvent<bool>();
+
+    public UnityEvent<bool> OnVersionCheckComplete = new UnityEvent<bool>();
 
     private void Start()
     {
-        if (InternetAvailability.IsConnectedToInternet() == true)
+        if(InternetAvailability.IsConnectedToInternet() == true)
         {
-
-            OnVersionCheckComplete.AddListener(HandleVersionCheckComplete);
-
             StartCoroutine(AccessVersionURL());
         }
         else
         {
-            Debug.LogWarning("No internet connection");
-            checkVersionUpdateCanvas.gameObject.SetActive(false);
-            noInternetCanvas.gameObject.SetActive(true);
-            //todo: put procedures for no internet connection
+            //not connected to internet prompt thing
+            //put it here later lol
         }
-    }
-
-    //called when version check is complete - not meant to be called otherwise
-    void HandleVersionCheckComplete(bool isOutdated)
-    {
-        if (isOutdated)
-        {
-            Debug.LogWarning("App is outdated!");
-            checkVersionUpdateCanvas.gameObject.SetActive(false);
-            outOfDateCanvas.gameObject.SetActive(true);
-        }
-        else
-        {
-            Debug.Log("App is up to date!");
-            //todo: proceed to next scene
-        }
-
-        OnVersionCheckComplete.RemoveListener(HandleVersionCheckComplete); //unsubscribe to event
     }
 
     /// <summary>
